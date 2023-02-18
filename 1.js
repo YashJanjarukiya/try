1dc6d1ffb8e7ce8c4fc1a60131c0729e83c4b58c
YashJanjarukiya <yash4410@gmail.com>
2023-02-18 12:37:31 +0530

    demo

---
+++ 1.js
@@ -0,0 +1,6 @@
+var a = 100;
+var b = 2;
+multiplication(a, b);
+function multiplication(a, b) {
+    console.log("multiplication:" + (a * b));
+}
---
+++ 1.ts
@@ -0,0 +1,11 @@
+var a:number=100;
+
+var b:number=2;
+
+multiplication(a,b);
+
+function multiplication(a,b)
+{
+    console.log("multiplication:"+(a*b));
+
+}
