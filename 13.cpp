b13125b0cf98802e82fd745b0de6662d6b79ab60
YashJanjarukiya <yash4410@gmail.com>
2023-02-24 22:11:42 +0530

    add

---
+++ 13.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,num;
+	cout<<"enter the a";
+	cin>>a;
+	
+	cout<<"enter the b";
+	cin>>b;
+	
+	int ans=a+b;
+	
+	cout<<"the sum is\n"<<ans;
+}
